9be7d85fee8a4c65bccc0d89204d153d57d28b38
Sharat Maharjan <sharatmaharjan@gmail.com>
2024-11-09 16:36:59 +0545

    Create script.js

---
+++ 12/Template/Javascript/script.js
@@ -0,0 +1,24 @@
+// Function to calculate the area of the rectangle
+function calculateAreaOfRectangle() {
+    
+}
+
+// Function to calculate Simple Interest
+function calculateSimpleInterest() {
+    
+}
+
+// Function to calculate the area of the circle
+function calculateAreaOfCircle() {
+    
+}
+
+// Function to calculate BMI
+function calculateBMI() {
+    
+}
+
+// Function to convert Celsius to Fahrenheit
+function convertToFahrenheit() {
+    
+}
