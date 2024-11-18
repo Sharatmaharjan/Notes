ae5eba2e9c8f4611aa44f8ec62a8da02b8158994
Sharat Maharjan <sharatmaharjan@gmail.com>
2024-11-18 08:15:34 +0545

    Create db.php

---
+++ 12/Template/PHP/login/db.php
@@ -0,0 +1,15 @@
+<?php
+// Database configuration
+$host = 'localhost';
+$dbname = 'prime';
+$username = 'root';
+$password = '';
+
+// Connect to the database
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+?>
